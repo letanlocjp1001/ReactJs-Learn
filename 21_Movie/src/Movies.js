10617c5b00e0cad1f7f478f2c79c7f15f59fcdbf
LE TAN LOC <letanlocjp1001@gmail.com>
2021-12-19 23:57:12 +0900

    update Movie Film

--- 21_Movie/src/Movies.js
+++ 21_Movie/src/Movies.js
@@ -5,7 +5,26 @@ const url =
   'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
 const Movies = () => {
-  return <h2>movies component</h2>
+  const { movies, isLoading } = useGlobalContext()
+  if (isLoading) {
+    return <div className='loading'></div>
+  }
+  return (
+    <section className='movies'>
+      {movies.map((movie) => {
+        const { Title: title, Poster: poster, Year: year, imdbID: id } = movie
+        return (
+          <Link to={`/movies/${id}`} key={id} className='movie'>
+            <article>
+              <img src={poster === 'N/A' ? url : poster} alt={title} />
+              <div className='title'>{title}</div>
+              <p>{year}</p>
+            </article>
+          </Link>
+        )
+      })}
+    </section>
+  )
 }
 
 export default Movies
